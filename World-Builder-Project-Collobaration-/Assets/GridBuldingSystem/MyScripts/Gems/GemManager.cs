9e6e87f88b0e9eca3e6cf2b835a95247b51d404b
AnnFyz <annfyz1232@yandex.ru>
2023-01-23 19:55:28 +0100

    add gemspawning in waves

--- World-Builder-Project-Collobaration-/Assets/GridBuldingSystem/MyScripts/Gems/GemManager.cs
+++ World-Builder-Project-Collobaration-/Assets/GridBuldingSystem/MyScripts/Gems/GemManager.cs
@@ -9,30 +9,55 @@ public class GemManager : MonoBehaviour //make spawn in waves with particles
 {
     public List<GemsSO> gems = new List<GemsSO>();
     NavMeshSurface surface;
-
+    NavMeshTriangulation triangulation;
+    GameObject rainObj;
+    List<Transform> createdGems = new List<Transform>();
     private void Start()
     {
-        for (int i = 0; i < 20; i++)
-        {
-            Debug.Log(GetRandomGem().ToString());
-        }
-
         surface = GridOfPrefabs.Instance.horizontalSurface;
+        rainObj = transform.GetChild(0).gameObject;
+        rainObj.SetActive(false);
+        StartCoroutine(SpawnGemsInWaves());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnRandomGems(10);
+        }
+    }
+
+    IEnumerator SpawnGemsInWaves()
+    {
+        while (createdGems.Count <= 30)
+        {
+         rainObj.SetActive(true);
+         yield return new WaitForSeconds(5f);
+        for (int i = 0; i < 10; i++)
+        {
+          SpawnRandomGems(1);
+          yield return new WaitForSeconds(1f);
+        }
+         rainObj.SetActive(false);
+         yield return null;
+
+        }    
     }
     Transform GetRandomGem()
     {
         Transform gem = null;
         var totalWeight = 0;
-        foreach(var item in gems)
+        foreach (var item in gems)
         {
             totalWeight += item.weight;
         }
         var rndWeightGem = UnityEngine.Random.Range(0, totalWeight);
         var processedWeight = 0;
-        foreach(var item in gems)
+        foreach (var item in gems)
         {
             processedWeight += item.weight;
-            if(rndWeightGem <= processedWeight)
+            if (rndWeightGem <= processedWeight)
             {
                 gem = item.gemPrefab.transform;
                 break;
@@ -40,9 +65,48 @@ public class GemManager : MonoBehaviour //make spawn in waves with particles
         }
         return gem;
     }
-    void SpawnGems()
+    void SpawnRandomGems(int numberOfGems)
     {
+        triangulation = NavMesh.CalculateTriangulation();
+        int vertexIndex = UnityEngine.Random.Range(0, triangulation.vertices.Length);
         NavMeshHit hit;
+        Vector3 randomPos;
+        for (int i = 0; i < numberOfGems; i++)
+        {
+            randomPos = new Vector3(UnityEngine.Random.Range(-5, 5), 0, UnityEngine.Random.Range(-5, 5));
+            if (NavMesh.SamplePosition(triangulation.vertices[vertexIndex] + randomPos, out hit, 1f, -1))
+            {
+                Transform gem = Instantiate(GetRandomGem(), hit.position, Quaternion.identity, transform);
+                gem.GetComponent<NavMeshAgent>().Warp(hit.position);
+                gem.GetComponent<NavMeshAgent>().enabled = true;
+                createdGems.Add(gem);
+            }
+            else
+            {
+                SpawnRandomGem();
+            }
+        }
+
+    }
+
+    void SpawnRandomGem()
+    {
+        triangulation = NavMesh.CalculateTriangulation();
+        int vertexIndex = UnityEngine.Random.Range(0, triangulation.vertices.Length);
+        NavMeshHit hit;
+        Vector3 randomPos = new Vector3(UnityEngine.Random.Range(-5, 5), 0, UnityEngine.Random.Range(-5, 5));
+        if (NavMesh.SamplePosition(triangulation.vertices[vertexIndex] + randomPos, out hit, 1f, -1))
+        {
+            Transform gem = Instantiate(GetRandomGem(), hit.position, Quaternion.identity, transform);
+            gem.GetComponent<NavMeshAgent>().Warp(hit.position);
+            gem.GetComponent<NavMeshAgent>().enabled = true;
+            createdGems.Add(gem);
+        }
+        else
+        {
+            SpawnRandomGem();
+        }
+
     }
 
 }
