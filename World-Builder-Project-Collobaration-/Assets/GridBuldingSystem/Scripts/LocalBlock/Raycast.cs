952bba737bd87d65b9595c6eb05f06144aeee11d
Anna <64531297+AnnFyz@users.noreply.github.com>
2025-09-06 12:17:03 +0200

    refactored Raycast

--- World-Builder-Project-Collobaration-/Assets/GridBuldingSystem/Scripts/LocalBlock/Raycast.cs
+++ World-Builder-Project-Collobaration-/Assets/GridBuldingSystem/Scripts/LocalBlock/Raycast.cs
@@ -1,128 +1,113 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Raycast : MonoBehaviour // local raycasting for each block prefab
+
+// Handles local raycasting interactions for each BlockPrefab
+public class Raycast : MonoBehaviour
 {
 
-    BlockPrefab localPrefabBlock;
-    GridBuildingSystem localGrid;
-    Ray ray;
-    RaycastHit hit;
+    private BlockPrefab blockPrefab;    // The block this script is attached to
+    private GridBuildingSystem gridSystem;  // Local grid reference
+    private Outline outline;    // Cached outline component
+
+
     BlockPrefab block;
     private void Awake()
     {
-        localPrefabBlock = GetComponent<BlockPrefab>();
-        localGrid = GetComponent<GridBuildingSystem>();
+        blockPrefab = GetComponent<BlockPrefab>();
+        gridSystem = GetComponent<GridBuildingSystem>();
+        outline = GetComponent<Outline>();
     }
     private void Update()
     {
-        if (EventSystem.current.IsPointerOverGameObject())
+        // Skip if pointer is over UI
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            return;
+            HandleRaycastHit(hit);
         }
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
-        {
-            //the collider could be children of the unit, so we make sure to check in the parent
-            block = hit.collider.GetComponentInParent<BlockPrefab>();
-            if (localPrefabBlock == block)
-            {
-                localPrefabBlock.isHighlighted = true;
-                SetOutline(true);
-                if (localPrefabBlock.isSelected && localPrefabBlock.isHighlighted)
-                {
-                    //localPrefabBlock.ChangeSelectedMaterial();
-                    if (BuildingManager.Instance.currentObjectTypeSO == null)
-                    {
-                        BuildingManager.Instance.currentObjectTypeSO = BuildingManager.Instance.lastSelectedObjToPlaceTypeSO;
-                    }
-
-                        BuildingManager.localGrid = localGrid.Grid;
-                        BuildingManager.Instance.lastBlockPrefab = BuildingManager.Instance.currentBlockPrefab;
-                        BuildingManager.Instance.currentBlockPrefab = localPrefabBlock;     
-                       if (BuildingManager.Instance.lastBlockPrefab != null && BuildingManager.Instance.currentBlockPrefab.GetComponent<LocalLevelState>().GetCurrentLevelState() == BuildingManager.Instance.lastBlockPrefab.GetComponent<LocalLevelState>().GetCurrentLevelState())
-                        {
-                        BuildingManager.Instance.RefreshSelectedObjectType();
-                    }
-                    else
-                    {
-                        BuildingManager.Instance.DeselectObjectTypeOnSelectedAnotherBlockType();
-                    }
-                      
-
-
-                }
-                else
-                {
-                    BuildingManager.Instance.DeselectObjectType();
-                }
-            }
-            else
-            {
-               
-                localPrefabBlock.isHighlighted = false;
-                SetOutline(false);
-                //BuildingManager.Instance.DeselectObjectType();
-
-                if (!localPrefabBlock.isSelected)
-                {
-           
-                    //localPrefabBlock.ChangeColorBack();
-                   // localPrefabBlock.ChangeMaterialBack();
-                    if(localPrefabBlock.GetComponent<LocalLevelState>().GetCurrentLevelState() == LevelState.Forest)
-                    {
-                    }
-                }
-                else
-                {
-                  //  localPrefabBlock.ChangeSelectedMaterial();
-                }
-                
-            }
-        }
-
         else
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                localPrefabBlock.isHighlighted = false;
-                //SetOutline(false);
-                localPrefabBlock.isSelected = false;
-               // localPrefabBlock.ChangeColorBack();
-               // localPrefabBlock.ChangeMaterialBack();
-                BuildingManager.Instance.DeselectObjectType();
-                UIManager.Instance.LocalSetupUIIcons();
-            }
-           
-
-            //if (!localPrefabBlock.IsThisBlockIsSelected)
-            //{
-            //    localPrefabBlock.ChangeColorBack();
-            //    localPrefabBlock.ChangeMaterialBack();
-            //    BuildingManager.Instance.DeselectObjectType();
-            //}
-            //else
-            //{
-            //    localPrefabBlock.ChangeSelectedMaterial();
-            //}
-        } 
+            HandleRaycastMiss();
+        }
     }
 
-    public void SetOutline(bool isSelected)
+    // Process logic when raycast hits something
+    private void HandleRaycastHit(RaycastHit hit)
     {
-        if (isSelected)
+        BlockPrefab hitBlock = hit.collider.GetComponentInParent<BlockPrefab>();
+        bool isThisBlock = (blockPrefab == hitBlock);
+
+        blockPrefab.isHighlighted = isThisBlock;
+        SetOutline(isThisBlock);
+
+        if (!isThisBlock) return;
+
+        if (blockPrefab.isSelected && blockPrefab.isHighlighted)
         {
-            GetComponent<Outline>().enabled = true;
-            GetComponent<Outline>().OutlineColor = Color.white;
-            GetComponent<Outline>().OutlineWidth = 3.5f;
+            HandleBlockSelection();
         }
         else
         {
-            if(!localPrefabBlock.isSelected)
-            GetComponent<Outline>().enabled = false;
+            BuildingManager.Instance.DeselectObjectType();
+        }
+    }
+
+    // Process logic when raycast hits nothing
+    private void HandleRaycastMiss()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        blockPrefab.isHighlighted = false;
+        blockPrefab.isSelected = false;
+
+        BuildingManager.Instance.DeselectObjectType();
+        UIManager.Instance.LocalSetupUIIcons();
+    }
+
+    // Handles updating BuildingManager when a block is selected
+    private void HandleBlockSelection()
+    {
+        if (BuildingManager.Instance.currentObjectTypeSO == null)
+        {
+            BuildingManager.Instance.currentObjectTypeSO = BuildingManager.Instance.lastSelectedObjToPlaceTypeSO;
+        }
+
+        BuildingManager.localGrid = gridSystem.Grid;
+        BuildingManager.Instance.lastBlockPrefab = BuildingManager.Instance.currentBlockPrefab;
+        BuildingManager.Instance.currentBlockPrefab = blockPrefab;
+
+        // Refresh or reset selection depending on block type
+        if (BuildingManager.Instance.lastBlockPrefab != null &&
+            blockPrefab.GetComponent<LocalLevelState>().GetCurrentLevelState() ==
+            BuildingManager.Instance.lastBlockPrefab.GetComponent<LocalLevelState>().GetCurrentLevelState())
+        {
+            BuildingManager.Instance.RefreshSelectedObjectType();
+        }
+        else
+        {
+            BuildingManager.Instance.DeselectObjectTypeOnSelectedAnotherBlockType();
+        }
+    }
+
+    // Enable/disable outline visuals
+    public void SetOutline(bool enable)
+    {
+        if (outline == null) return;
+        if (enable)
+        {
+            outline.enabled = true;
+            outline.OutlineColor = Color.white;
+            outline.OutlineWidth = 3.5f;
+        }
+        else if (!blockPrefab.isSelected)
+        {
+            outline.enabled = false;
         }
     }
 }
